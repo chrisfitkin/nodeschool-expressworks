c3b8448951276182aa98fd6e5cbd5cb776a75e3e
chrisfitkin <cfitkin@gmail.com>
2016-09-14 00:22:13 +0000

    form/bodyparser (incomplete)

--- program.js
+++ program.js
@@ -2,6 +2,7 @@
   var express = require('express');
   var path = require('path');
   var jade = require('jade');
+  var bodyparser = require('body-parser');
   var app = express();
 
   // ROUTING
@@ -13,14 +14,23 @@
   // app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
 
   // JADE TEMPLATE
-  console.log(process.argv[3]);
-  app.set('views', process.argv[3]);
-  app.set('view engine', 'jade');
-  app.get('/home', function (req, res) {
-    res.render('index', {
-        date: new Date().toDateString(),
-    });
-  });
+  // console.log(process.argv[3]);
+  // app.set('views', process.argv[3]);
+  // app.set('view engine', 'jade');
+  // app.get('/home', function (req, res) {
+  //   res.render('index', {
+  //       date: new Date().toDateString(),
+  //   });
+  // });
+  //
 
+  // GOOD OLD FORM
+  app.use(bodyparser.urlencoded({extended: false}));
+  app.get('/form', function(req, res) {
+    res.end('<form><input name="str"/></form>');
+  });
+  app.post('/form', function(req, res) {
+    res.end(req.body.str.split('').reverse().join(''));
+  });
 
   app.listen(process.argv[2]);
