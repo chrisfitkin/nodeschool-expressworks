db7f32cc3832dd8eb215859c03e7353cecb70e0a
chrisfitkin <cfitkin@gmail.com>
2016-09-14 00:12:42 +0000

    jade

--- program.js
+++ program.js
@@ -1,9 +1,26 @@
 
   var express = require('express');
+  var path = require('path');
+  var jade = require('jade');
   var app = express();
+
+  // ROUTING
   // app.get('/home', function (req, res) {
   //   res.end('Hello World!');
   // });
-  app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+
+  // STATIC CONTENT
+  // app.use(express.static(process.argv[3] || path.join(__dirname, 'public')));
+
+  // JADE TEMPLATE
+  console.log(process.argv[3]);
+  app.set('views', process.argv[3]);
+  app.set('view engine', 'jade');
+  app.get('/home', function (req, res) {
+    res.render('index', {
+        date: new Date().toDateString(),
+    });
+  });
+
 
   app.listen(process.argv[2]);
